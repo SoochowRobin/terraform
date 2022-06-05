463818a0da9e34fbab8ec63a7fb54a8406ca7a05
Robin_Chen <55612410+SoochowRobin@users.noreply.github.com>
2022-06-05 14:56:56 -0400

    Add files via upload
    Attach one subnet to an instance

--- main.tf
+++ main.tf
@@ -13,9 +13,28 @@ provider "aws" {
 }
 
 # create a VPC
+# some of vpc would export 
 resource "aws_vpc" "main" {
     cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "robin vpc"
+    }
 }
+
+
+# define a subnet and it get vpc_id from vpc export
+
+resource "aws_subnet" "robinSubnet" {
+    # how to get a export from one resource, resourcetpye.name.param
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+
+    tags = {
+        Name = "robin subnet"
+    }
+}
+
 
 
 resource "aws_instance" "foobar" {
@@ -23,6 +42,9 @@ resource "aws_instance" "foobar" {
     # when you want to change ami you just change it in code
     ami = "ami-0f095f89ae15be883"
     instance_type = "t2.medium"
+
+    # attach subnet to this instance 
+    subnet_id = aws_subnet.robinSubnet.id
 
     tags = {
         Name = "HelloRobin"
